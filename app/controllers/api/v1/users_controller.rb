3550ccbd124e41460c2769f82e770e06656c5980
sampassarelli <passarelli.sam@gmail.com>
2021-01-29 13:54:16 -0600

    final push before presentations

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,17 @@
 class Api::V1::UsersController < ApplicationController
+  def create
+    user = User.new(user_params)
+    
+    if user.save
+      render json: user
+    else 
+      render json: {"User not created, please try again!"}
+    end
+  end
+
+  private
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
 end
